8e1db7d85c0a184743acd1921fabd5abab0367c5
Marie-Louise <mariemuteteyimana@gmail.com>
2021-03-23 11:49:01 +0300

    2rd assgnment

---
+++ src/Baby.kt
@@ -0,0 +1,27 @@
+fun main() {
+name()
+sum()
+    modulus()
+personality()
+
+}
+fun name(){
+    println("hello marie")
+}
+fun sum(){
+    var num1=3
+    var num2=6
+    var num3=6
+    var num4=4
+    var sum=num1+num2+num3+num4
+    println(sum)
+}
+fun modulus(){
+    var num1=20
+    var num2=3
+    var modulus=num1%num2
+    println(modulus)
+}
+fun personality(){
+    println("obedience")
+}
